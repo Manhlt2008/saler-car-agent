5a0456cfe702eff35c9e776a7dfdaee0180292e5
Dzung Nguyen <dungnt69@fpt.com>
2025-10-30 14:08:56 +0700

    separate chromeDB

---
+++ backend/chromaDBCall.py
@@ -0,0 +1,84 @@
+import os
+import chromadb
+from config import client, embedding_client
+from databaseCars import database_cars
+
+# use chromadb
+OPENAI_EMBEDDING_API_KEY = os.getenv("OPENAI_EMBEDDING_API_KEY")
+OPENAI_EMBEDDING_ENDPOINT = os.getenv("OPENAI_EMBEDDING_ENDPOINT")
+OPENAI_EMBEDDING_MODEL = os.getenv("OPENAI_EMBEDDING_MODEL")
+# ---- LLM CONFIG ----
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+OPENAI_ENDPOINT = os.getenv("OPENAI_ENDPOINT")
+DEPLOYMENT_NAME = os.getenv("DEPLOYMENT_NAME")
+
+# ---- GET EMBEDDING ----
+def get_embedding(text):
+    response = embedding_client.embeddings.create(input=text, model=OPENAI_EMBEDDING_MODEL)
+    return response.data[0].embedding
+
+# ---- CALL LLM ----
+def ask_llm(context, user_input):
+    system_prompt = """Bạn là một chuyên gia sale trong lĩnh vực mua bán xe hơi.
+        Nếu như câu hỏi là những thứ ngoài lĩnh vực này thì hãy trả lời là:
+        Xin lỗi bạn đây là câu hỏi nằm ngoài lĩnh vực của tôi. Xin hãy đặt lại câu hỏi."""
+    user_prompt = (
+        f"Yêu cầu người dùng: {user_input}\n\n"
+        f"Xe đề xuất:\n{context}\n\n"
+        "Dựa vào yêu cầu bên trên và thông tin xe đã cho, hãy đề xuất chiếc xe phù hợp nhất với người dùng."
+    )
+    messages = [
+        {"role": "system", "content": system_prompt},
+        {"role": "user", "content": user_prompt},
+    ]
+    response = client.chat.completions.create(model=DEPLOYMENT_NAME, messages=messages)
+    return response.choices[0].message.content
+
+
+# ---- CHROMADB ----
+chroma_client = chromadb.Client()
+collection = chroma_client.create_collection(name="database_cars")
+
+# ---- ADD CAR TO CHROMADB ----
+for car in database_cars:
+    var_embedding = f"""name: {car['name']} 
+                    brand: {car['brand']}
+                    price_min: {car['price_min']}
+                    price_max: {car['price_max']}
+                    segment: {car['segment']}
+                    seats: {car['seats']}
+                    fuel_type: {car['fuel_type']}
+                    transmission: {car['transmission']}
+                    """
+    embedding = get_embedding(var_embedding)
+    collection.add(
+        embeddings=[embedding],
+        documents=[car["features"]],
+        ids=[car["id"]],
+        metadatas=[{"name": car["name"], "brand": car["brand"], "image": car["image_url"]}],
+    )
+
+
+def build_context(results, n_context=3):
+    docs = results["documents"][0]
+    metas = results["metadatas"][0]
+    context_str = ""
+    for doc, meta in zip(docs, metas):
+        context_str += (
+            f"Name: {meta['name']}\n"
+            f"Description: {doc}\n"
+            f"Brand: {meta['brand']}\n"
+            f"Image: {meta['image']}\n\n"
+        )
+    return context_str.strip()
+
+
+def callChromaDB(user_input):
+    userInput = user_input[-1]["content"]
+    query_embedding = get_embedding(userInput)
+    results = collection.query(query_embeddings=[query_embedding], n_results=3)
+    # Step 2: Build context for LLM
+    context = build_context(results)
+    # Step 3: Get recommendation from LLM
+    llm_output = ask_llm(context, user_input)
+    return llm_output
